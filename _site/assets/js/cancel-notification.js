b32fa449cd9b0e85a9ef95c91f947bdfa7817021
Jacky Lim <jacky@somd.sg>
2021-09-30 18:42:33 +0700

    Removed generated _site folder from git. Fixed homepage banner button bottom margin.

--- _site/assets/js/cancel-notification.js
+++
@@ -1,7 +0,0 @@
-if (document.getElementById("cancel-notification")) {
-	document.getElementById("cancel-notification").addEventListener("click", hideNotification);
-}
-
-function hideNotification() {
-	document.getElementById("notification-bar").style.display = "none";
-}
